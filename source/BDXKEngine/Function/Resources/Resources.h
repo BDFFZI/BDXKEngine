7936ccf1b8a6a8d607e0e04a500eee9c27d4525b
BDFFZI <1330958515@qq.com>
2023-03-18 19:58:46 +0800

    优化仓库大小

---
+++ source/BDXKEngine/Function/Resources/Resources.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "BDXKEngine/Base/Object/Serializer/ObjectSerializer.h"
+
+namespace BDXKEngine
+{
+    class Resources
+    {
+    public:
+        static std::string ParsePath(const std::string& path);
+
+        static ObjectPtrBase Load(const std::string& path, bool retainPersistent = true);
+        template <typename TObject>
+        static ObjectPtr<TObject> Load(const std::string& path, bool retainPersistent = true)
+        {
+            return Load(path, retainPersistent).ToObject<TObject>();
+        }
+        static void Save(const std::string& path, const ObjectPtrBase& objectPtr);
+
+        static bool IsExisting(const std::string& path);
+
+        static ObjectPtrBase FindSerializationFallback(const Guid& guid);
+    private:
+        static void StaticConstructor();
+        CustomStaticConstructor(StaticConstructor)
+    };
+}
