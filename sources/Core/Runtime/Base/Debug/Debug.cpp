39d457bffe39a10da6715b0131e9dabe507f5afb
夷乐鑫 <1330958515@qq.com>
2024-09-18 11:39:36 +0800

    修复git意外忽略Debug模块的问题

---
+++ sources/Core/Runtime/Base/Debug/Debug.cpp
@@ -0,0 +1,27 @@
+﻿#include "Debug.h"
+#include <iostream>
+#include <regex>
+
+namespace BDXKEngine
+{
+    void Debug::Log(const std::string& log)
+    {
+        std::cout << log << std::endl;
+    }
+    void Debug::LogWarning(const std::string& warning)
+    {
+        std::cout << "[!]" << warning << std::endl;
+    }
+    void Debug::LogError(const std::string& error)
+    {
+        std::cout << "[X]" << error << std::endl;
+    }
+    void Debug::LogError(const int error_code, const char* description)
+    {
+        std::cout << "[X]" << error_code << ':' << description << std::endl;
+    }
+    void Debug::LogException(const std::exception& exception)
+    {
+        std::cout << "[X]" << exception.what() << std::endl;
+    }
+}
